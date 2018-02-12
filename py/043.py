c387def9fe12cde6dd5823dd776daf7f4d37bfa5
Trey Frank Boehm <trey.frank.boehm@gmail.com>
2018-02-11 20:23:27 -0600

    Python solution to 43 that runs in ~2 minutes

--- py/043.py
+++ py/043.py
@@ -1,20 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+primes = [2, 3, 5, 7, 11, 13, 17]
 
 def isPandigital(number):
     number = str(number)
-    if len(number) > 10: return False
+    if len(number) != 10: return False
     for x in range(10):
         if not str(x) in number: return False
     return True
 
-def subStringDivisibility(number):
+def specialProperty(number):
     if not isPandigital(number): return 0
-    number = str(number)
-    if int(number[1:4]) % 2 == 0 and int(number[2:5]) % 3 == 0 and int(number[3:6]) % 5 == 0:
-        if int(number[4:7]) % 7 == 0 and int(number[5:8]) % 11 == 0 and int(number[6:9]) % 13 == 0:
-            if int(number[7:10]) % 17 == 0:
-                return int(number)
-    return 0
+
+    for i in range(len(primes)-1,-1,-1):
+        if not subStringDivisibility(number, i+1, i+4, primes[i]):
+            return False
+    return True
+
+def subString(number, dig1, dig2):
+    number = number // (10**(10-dig2))
+    number = number % (10**(dig2-dig1))
+    return number
+
+def subStringDivisibility(number, dig1, dig2, factor):
+    return (subString(number, dig1, dig2) % factor == 0)
 
 def makeDivisible(number, factor):
     newNum = ((factor - number)%2)
@@ -22,19 +31,19 @@ def makeDivisible(number, factor):
     return newNum
 
 
-#count = "1406357289"
-#print count[3], count[4], count[5], count[6], count[7], count[8], count[9]
+if __name__ == "__main__":
+    total = 0
+    count = int(1e9)
+    while count <= int(1e10):
+        if specialProperty(count):
+            total += count
 
-total = 0
-count = 1000000000
-while count <= 10000000000:
-    count += makeDivisible(int(str(count)[1:4]), 2)*10**6
-    count += makeDivisible(int(str(count)[2:5]), 3)*10**5
-    count += makeDivisible(int(str(count)[3:6]), 5)*10**4
-    count += makeDivisible(int(str(count)[4:7]), 7)*10**3
-    count += makeDivisible(int(str(count)[5:8]), 11)*10**2
-    count += makeDivisible(int(str(count)[6:9]), 13)*10**1
-    count += makeDivisible(int(str(count)[7:10]), 17)*10**0
-    total += subStringDivisibility(count)
+        # Increment by 17 to keep the last 3 digits divisible, unless
+        # we're about to wrap around (at 986 -> 1000 instead of 1003)
+        if count % 1000 == 986:
+            count += 14
+        else:
+            count += 17
 
-print total
+    print(total)
+
